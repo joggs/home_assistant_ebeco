3d94d1af34a69c7796c28b164e7e652178ebfce2
TurboJonte <turbojonte@gmail.com>
2022-03-05 10:16:31 +0100

    Update sensor.py
    #17 Added diagnostic sensor for installed power and fixed language error inherited from the API

--- custom_components/ebeco/sensor.py
+++ custom_components/ebeco/sensor.py
@@ -33,6 +33,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     dev = []
     dev.append(EbecoRelaySensor(instance, device_data, sensor))
     dev.append(EbecoPowerSensor(instance, device_data, sensor))
+    dev.append(EbecoInstalledPowerSensor(instance, device_data, sensor))
     dev.append(EbecoEnergySensor(instance, device_data, sensor))
     dev.append(EbecoTemperatureSensor(instance, device_data, "Floor"))
     dev.append(EbecoTemperatureSensor(instance, device_data, "Room"))
@@ -105,8 +106,8 @@ class EbecoPowerSensor(EbecoEntity, SensorEntity):
         return POWER_WATT
 
     @property
-    def installed_effect(self):
-        """Return the installed effect in Watts."""
+    def installed_power(self):
+        """Return the installed power in Watts."""
         return self._device["installedEffect"]
 
     def is_on(self) -> bool:
@@ -117,9 +118,54 @@ class EbecoPowerSensor(EbecoEntity, SensorEntity):
     def native_value(self) -> StateType:
         """Return the state of the entity."""
         if self.is_on():
-            return self.installed_effect
+            return self.installed_power
 
         return 0
+
+class EbecoInstalledPowerSensor(EbecoEntity, SensorEntity):
+    def __init__(self, instance, device_data, sensor):
+        """Initialize the thermostat."""
+        super().__init__(instance, device_data["id"], sensor)
+        self.main_sensor = MAIN_SENSOR
+
+    @property
+    def device_class(self) -> str:
+        """Return device class."""
+        return DEVICE_CLASS_POWER
+
+    @property
+    def state_class(self) -> str:
+        """Return the state class of this entity."""
+        return STATE_CLASS_MEASUREMENT
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return f"{self._device['id']}-installed-power"
+
+    @property
+    def name(self):
+        """Return the name of the device, if any."""
+        return f"{self._device['displayName']} Installed Power"
+
+    @property
+    def entity_category(self) -> str:
+        return ENTITY_CATEGORY_DIAGNOSTIC
+
+    @property
+    def native_unit_of_measurement(self) -> str:
+        """Return the unit of measurement of this entity."""
+        return POWER_WATT
+
+    @property
+    def installed_power(self):
+        """Return the installed power in Watts."""
+        return self._device["installedEffect"]
+
+    @property
+    def native_value(self) -> StateType:
+        """Return the state of the entity."""
+        return self.installed_power
 
 
 class EbecoEnergySensor(EbecoEntity, SensorEntity):
@@ -163,16 +209,16 @@ class EbecoEnergySensor(EbecoEntity, SensorEntity):
         return self._device["todaysOnMinutes"]
 
     @property
-    def installed_effect(self):
-        """Return the installed effect in Watts."""
+    def installed_power(self):
+        """Return the installed power in Watts."""
         return self._device["installedEffect"]
 
     @property
     def native_value(self) -> StateType:
         """Return the state of the entity."""
         minutes = self.todays_on_minutes
-        effect = self.installed_effect
-        return self.formatter((minutes / 60) * (effect / 1000))
+        power = self.installed_power
+        return self.formatter((minutes / 60) * (power / 1000))
 
     def formatter(self, value):
         """Numeric pass-through formatter."""
@@ -181,8 +227,7 @@ class EbecoEnergySensor(EbecoEntity, SensorEntity):
                 float(value * self._multiplier) / self._divisor, self._decimals
             )
         return round(float(value * self._multiplier) / self._divisor)
-
-
+    
 class EbecoTemperatureSensor(EbecoEntity, SensorEntity):
     def __init__(self, instance, device_data, sensor):
         """Initialize the thermostat temperature sensor."""
